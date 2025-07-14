31a5e8d7c26a6642d89711eeed1171034089495b
winter <3227163866@qq.com>
2025-07-15 00:30:46 +0800

    CMakeLists.txt

---
+++ build/lib/CMakeFiles/ImmersedBoundaryMethod.dir/cmake_clean.cmake
@@ -0,0 +1,35 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ImmersedBoundaryMethod.dir/IBModel/IBModel.C.o"
+  "CMakeFiles/ImmersedBoundaryMethod.dir/IBModel/IBModel.C.o.d"
+  "CMakeFiles/ImmersedBoundaryMethod.dir/IBModel/directForcing/directForcing.C.o"
+  "CMakeFiles/ImmersedBoundaryMethod.dir/IBModel/directForcing/directForcing.C.o.d"
+  "CMakeFiles/ImmersedBoundaryMethod.dir/IBMotions/IBMotion/IBMotion.C.o"
+  "CMakeFiles/ImmersedBoundaryMethod.dir/IBMotions/IBMotion/IBMotion.C.o.d"
+  "CMakeFiles/ImmersedBoundaryMethod.dir/IBMotions/IBMotion/fixedLinearMotion/fixedLinearMotion.C.o"
+  "CMakeFiles/ImmersedBoundaryMethod.dir/IBMotions/IBMotion/fixedLinearMotion/fixedLinearMotion.C.o.d"
+  "CMakeFiles/ImmersedBoundaryMethod.dir/IBMotions/IBMotion/sixDoFMotion/sixDoFMotion.C.o"
+  "CMakeFiles/ImmersedBoundaryMethod.dir/IBMotions/IBMotion/sixDoFMotion/sixDoFMotion.C.o.d"
+  "CMakeFiles/ImmersedBoundaryMethod.dir/IBMotions/IBMotionSolver/IBMotionSolver.C.o"
+  "CMakeFiles/ImmersedBoundaryMethod.dir/IBMotions/IBMotionSolver/IBMotionSolver.C.o.d"
+  "CMakeFiles/ImmersedBoundaryMethod.dir/IBMotions/IBMotionSolver/modifiedNewton/modifiedNewton.C.o"
+  "CMakeFiles/ImmersedBoundaryMethod.dir/IBMotions/IBMotionSolver/modifiedNewton/modifiedNewton.C.o.d"
+  "CMakeFiles/ImmersedBoundaryMethod.dir/IBMotions/IBMotionSolver/newton/newton.C.o"
+  "CMakeFiles/ImmersedBoundaryMethod.dir/IBMotions/IBMotionSolver/newton/newton.C.o.d"
+  "CMakeFiles/ImmersedBoundaryMethod.dir/IBOStream/IBOStream.C.o"
+  "CMakeFiles/ImmersedBoundaryMethod.dir/IBOStream/IBOStream.C.o.d"
+  "CMakeFiles/ImmersedBoundaryMethod.dir/IBObject/IBObject.C.o"
+  "CMakeFiles/ImmersedBoundaryMethod.dir/IBObject/IBObject.C.o.d"
+  "CMakeFiles/ImmersedBoundaryMethod.dir/IBObject/IBParticle/IBParticle.C.o"
+  "CMakeFiles/ImmersedBoundaryMethod.dir/IBObject/IBParticle/IBParticle.C.o.d"
+  "CMakeFiles/ImmersedBoundaryMethod.dir/eulerMesh/eulerMesh.C.o"
+  "CMakeFiles/ImmersedBoundaryMethod.dir/eulerMesh/eulerMesh.C.o.d"
+  "CMakeFiles/ImmersedBoundaryMethod.dir/immersedBoundaryMethod/immersedBoundaryMethod.C.o"
+  "CMakeFiles/ImmersedBoundaryMethod.dir/immersedBoundaryMethod/immersedBoundaryMethod.C.o.d"
+  "libImmersedBoundaryMethod.a"
+  "libImmersedBoundaryMethod.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ImmersedBoundaryMethod.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
